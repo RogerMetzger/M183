83cd3806794d44723049bff46215578528dbf79f
RogerMetzger <roger.metzger@hotmail.com>
2017-09-22 11:19:00 +0200

    one time password verifizieren

--- TimebasedOneTimePassword/index.php
+++ TimebasedOneTimePassword/index.php
@@ -12,6 +12,27 @@
     require_once('otphp/otphp.php');
     $totp = new \OTPHP\TOTP("JBSWY3DPEHPK3PXP");
     echo "Current OTP: ". $totp->now();
+
+    function Check(){
+        echo($totp->verify($totp->now()));
+    }
+
+    if(isset($POST['Token']))
+    {
+        echo("Enter form event");
+        if($totp->verify($POST['Token']))
+        {
+            echo("Token Matched");
+        }
+        else
+        {
+            echo("Token did not Matched");
+        }
+    }
     ?>
+    <form action="index.php">
+        <input type="text" name="Token" /> 
+        <input type="submit"></input>
+    </form>
 </body>
 </html>
